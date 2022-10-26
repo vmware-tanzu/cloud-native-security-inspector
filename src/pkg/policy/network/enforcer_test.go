6e9c6778c894fd447e46fe8c3c5204f154259a8e
zsimon <zsimon@vmware.com>
2022-10-26 14:24:36 +0800

    Update the import URL in unit test

--- src/pkg/policy/network/enforcer_test.go
+++ src/pkg/policy/network/enforcer_test.go
@@ -8,7 +8,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"goharbor.io/k8s-security-inspector/api/v1alpha1"
+	"github.com/vmware-tanzu/cloud-native-security-inspector/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
