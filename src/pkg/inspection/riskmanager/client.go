84b8b621373f8dacf1c04f7ea1b806bd96149a16
zy <piccolo_su@gmail.com>
2022-11-21 15:28:54 -0800

    add logic of evaluation

---
+++ src/pkg/inspection/riskmanager/client.go
@@ -0,0 +1,83 @@
+package riskmanager
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/go-logr/logr"
+	"net/http"
+)
+
+// Client client to post requests
+type Client struct {
+	conf   *Config
+	logger logr.Logger
+}
+
+// NewClient new client
+func NewClient(conf *Config, logger logr.Logger) *Client {
+	if conf == nil {
+		conf = DefaultConfig()
+	}
+	return &Client{conf: conf, logger: logger}
+}
+
+// IsAnalyzeRunning get analyze status
+func (c *Client) IsAnalyzeRunning() (bool, error) {
+	requestURL := fmt.Sprintf(fmt.Sprintf("%s/status", c.conf.Server))
+	res, err := http.Get(requestURL)
+	if err != nil {
+		return false, err
+	}
+
+	defer res.Body.Close()
+	var target Status
+
+	err = json.NewDecoder(res.Body).Decode(target)
+	if err != nil {
+		return false, err
+	}
+
+	return target.IsRunning, nil
+}
+
+// PostAnalyze ask server to analyze resources
+func (c *Client) PostAnalyze(a AnalyzeOption) error {
+	requestURL := fmt.Sprintf(fmt.Sprintf("%s/analyze", c.conf.Server))
+	c.logger.Info(fmt.Sprintf("post to %s", requestURL))
+
+	if jsonData, err := json.Marshal(a); err == nil {
+		request, error := http.NewRequest("POST", requestURL, bytes.NewBuffer(jsonData))
+		request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+		client := &http.Client{}
+		response, error := client.Do(request)
+		if error != nil || response.StatusCode != http.StatusCreated {
+			return err
+		}
+
+		response.Body.Close()
+	}
+
+	return nil
+}
+
+func (c *Client) PostResource(a interface{}) error {
+	requestURL := fmt.Sprintf(fmt.Sprintf("%s/resource", c.conf.Server))
+	c.logger.Info(fmt.Sprintf("post to %s", requestURL))
+
+	if jsonData, err := json.Marshal(a); err == nil {
+		request, error := http.NewRequest("POST", requestURL, bytes.NewBuffer(jsonData))
+		request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+		client := &http.Client{}
+		response, error := client.Do(request)
+		if error != nil || response.StatusCode != http.StatusCreated {
+			return err
+		}
+
+		response.Body.Close()
+	}
+
+	return nil
+}
---
+++ src/pkg/inspection/riskmanager/server.go
@@ -0,0 +1,149 @@
+package riskmanager
+
+import (
+	"github.com/gin-gonic/gin"
+	consumers "github.com/vmware-tanzu/cloud-native-security-inspector/pkg/data/consumers/opensearch"
+	"net/http"
+)
+
+// Status risk evaluation status
+type Status struct {
+	IsRunning bool  `json:"is_running"`
+	Last      int64 `json:"last"`
+}
+
+// AnalyzeOption analyze option
+type AnalyzeOption struct {
+	DumpDetails      bool
+	DumpAssessReport bool
+	SkipImages       []string
+}
+
+// Server the server
+type Server struct {
+	Workloads          Workloads
+	IsRunning          bool
+	Images             map[string]*ImageItem
+	Last               int64
+	DetailExporter     *consumers.OpenSearchExporter
+	AssessmentExporter *consumers.OpenSearchExporter
+}
+
+// NewServer new server instance
+func NewServer(detailExporter *consumers.OpenSearchExporter, assessmentExporter *consumers.OpenSearchExporter) *Server {
+	return &Server{DetailExporter: detailExporter, AssessmentExporter: assessmentExporter}
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func (s *Server) postResource(c *gin.Context) {
+	var v ResourceItem
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&v); err != nil {
+		return
+	}
+
+	images := v.GetImages()
+	for _, i := range images {
+		if im, ok := s.Images[i.UUID()]; !ok {
+			s.Images[i.UUID()] = im
+		}
+
+		s.Images[i.UUID()].AddRelatedResource(&v)
+	}
+
+	s.Workloads.AddResource(&v)
+
+	c.IndentedJSON(http.StatusCreated, v)
+}
+
+func (s *Server) Analyze(option AnalyzeOption) {
+	s.IsRunning = true
+	defer func() {
+		s.IsRunning = false
+	}()
+
+	var v RiskCollection
+	for _, t := range s.Images {
+		if err := t.FetchHarborReport(); err != nil {
+			//TODO handle error
+		} else {
+			// TODO generate vulnerability risk
+		}
+
+		for _, r := range t.Related {
+			var risks []*RiskItem
+			if r.Type == "Service" {
+				//TODO generate exposure risk
+			} else if r.Type == "Secret" {
+				//TODO generate host risk
+			} else if r.Type == "Node" {
+				//TODO generate compliance risk
+			} else if r.Type == "Secret" {
+				//TODO generate compliance risk
+			}
+
+			if s, ok := v[r.UUID()]; !ok {
+				v[r.UUID()] = risks
+			} else {
+				s = append(s, risks...)
+			}
+		}
+	}
+
+	s.Workloads.risks = v
+	if option.DumpAssessReport {
+		err := s.Workloads.ExportAssessmentDetails(s.DetailExporter)
+		if err != nil {
+
+		}
+	}
+
+	if option.DumpAssessReport {
+		err := s.Workloads.ExportAssessmentDetails(s.DetailExporter)
+		if err != nil {
+
+		}
+	}
+}
+
+// postAnalyze adds an album from JSON received in the request body.
+func (s *Server) postAnalyze(c *gin.Context) {
+	var v AnalyzeOption
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&v); err != nil {
+		return
+	}
+
+	if !s.IsRunning {
+		go s.Analyze(v)
+		c.IndentedJSON(http.StatusCreated, "start analyzing")
+	}
+
+	c.IndentedJSON(http.StatusFailedDependency, "analyze is running")
+}
+
+func (s *Server) getStatus(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Status{
+		IsRunning: s.IsRunning,
+		Last:      s.Last,
+	})
+}
+
+func (s *Server) getRisks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, s.Workloads)
+}
+
+// Run turn the server
+func (s *Server) Run(address string) {
+	s.Images = make(map[string]*ImageItem)
+	router := gin.Default()
+	router.GET("/status", s.getStatus)
+	router.GET("/risks", s.getRisks)
+	router.POST("/analyze", s.postAnalyze)
+	router.POST("/resource", s.postResource)
+	router.Run(address)
+}
