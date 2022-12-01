f8429eca092aeba2e4269bb1f83b491dde1e8036
qijianshuai <qjianshuai@vmware.com>
2022-12-01 11:38:10 +0800

    arksec-risk-portal modify

--- src/frontend/src/app/view/policy/policy-setting-page/policy-setting-page.component.ts
+++ src/frontend/src/app/view/policy/policy-setting-page/policy-setting-page.component.ts
@@ -288,20 +288,20 @@ export class PolicySettingPageComponent implements OnInit {
           this.policyForm.get('inspectionSetting')?.get('suspend')?.setValue(policyList[0].spec.strategy.suspend)
           this.policyForm.get('inspectionSetting')?.get('concurrencyRule')?.setValue(policyList[0].spec.strategy.concurrencyRule)
           if (policyList[0].spec.inspector.image && policyList[0].spec.inspector.kubebenchImage) {
-            if (policyList[0].spec.inspector.arksecRiskImage) {
+            if (policyList[0].spec.inspector.riskImage) {
               this.policyForm.get('inspectionSetting')?.get('image')?.setValue(['inspector', 'kubebench', 'risk'])
             } else {
               this.policyForm.get('inspectionSetting')?.get('image')?.setValue(['inspector', 'kubebench'])
             }
           } else {
             if (policyList[0].spec.inspector.image) {
-              if (policyList[0].spec.inspector.arksecRiskImage) {
+              if (policyList[0].spec.inspector.riskImage) {
                 this.policyForm.get('inspectionSetting')?.get('image')?.setValue(['inspector', 'risk'])
               } else {
                 this.policyForm.get('inspectionSetting')?.get('image')?.setValue(['inspector'])
               }
             } else if (policyList[0].spec.inspector.kubebenchImage) {
-              if (policyList[0].spec.inspector.arksecRiskImage) {
+              if (policyList[0].spec.inspector.riskImage) {
                 this.policyForm.get('inspectionSetting')?.get('image')?.setValue(['kubebench', 'risk'])
               } else {
                 this.policyForm.get('inspectionSetting')?.get('image')?.setValue(['kubebench'])
@@ -535,7 +535,7 @@ export class PolicySettingPageComponent implements OnInit {
         } else if (image === 'kubebench') {
           data.spec.inspector.kubebenchImage = this.imageList[1].url
         } else if (image === 'risk') {
-          data.spec.inspector.arksecRiskImage = this.imageList[2].url
+          data.spec.inspector.riskImage= this.imageList[2].url
         }
       });
     }
