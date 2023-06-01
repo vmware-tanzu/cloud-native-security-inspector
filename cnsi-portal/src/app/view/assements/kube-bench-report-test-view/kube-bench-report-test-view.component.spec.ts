d47ee5163633b9b852d9bebb43c3c8c21970c821
qijianshuai <qjianshuai@vmware.com>
2023-06-01 22:10:10 +0800

    update ui_ui

--- cnsi-portal/src/app/view/assements/kube-bench-report-test-view/kube-bench-report-test-view.component.spec.ts
+++ cnsi-portal/src/app/view/assements/kube-bench-report-test-view/kube-bench-report-test-view.component.spec.ts
@@ -3,13 +3,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AssessmentService } from 'src/app/service/assessment.service';
 import { ShardTestModule } from 'src/app/shard/shard/shard.module';
-import { KubeBenchReportListComponent } from '../kube-bench-report-list/kube-bench-report-list.component';
-import { KubeBenchReportTestDetailComponent } from '../kube-bench-report-test-detail/kube-bench-report-test-detail.component';
-import { KubeBenchReportComponent } from '../kube-bench-report/kube-bench-report.component';
-import { ReportViewComponent } from '../report-view/report-view.component';
-import { RiskReportViewComponent } from '../risk-report-view/risk-report-view.component';
-import { TrivyViewComponent } from '../trivy-view/trivy-view.component';
-
 import { KubeBenchReportTestViewComponent } from './kube-bench-report-test-view.component';
 
 describe('KubeBenchReportTestViewComponent', () => {
@@ -21,49 +14,8 @@ describe('KubeBenchReportTestViewComponent', () => {
       declarations: [ KubeBenchReportTestViewComponent ],
       imports: [ShardTestModule, RouterTestingModule.withRoutes(
         [{
-          path: 'assessments',
-          children: [
-            {
-              path: 'report',
-              component: ReportViewComponent
-            },
-            {
-              path: 'kube-bench',
-              component: KubeBenchReportComponent,
-              children:[
-                {
-                  path: 'list',
-                  component: KubeBenchReportListComponent
-                },
-                {
-                  path: 'test-view/:id',
-                  component: KubeBenchReportTestViewComponent
-                },
-                {
-                  path: 'test-detail/:id',
-                  component: KubeBenchReportTestDetailComponent
-                },
-                {
-                  path: '',
-                  pathMatch: 'full',
-                  redirectTo: 'list'        
-                }
-              ]
-            },
-            {
-              path: 'risk',
-              component: RiskReportViewComponent
-            },
-            {
-              path: 'trivy',
-              component: TrivyViewComponent
-            },
-            {
-              path: '',
-              pathMatch: 'full',
-              redirectTo: 'report'    
-            }
-          ]
+          path: 'assessments/kube-bench/test-detail/test',
+          component: KubeBenchReportTestViewComponent
         }]
       )],
       providers: [AssessmentService,],
